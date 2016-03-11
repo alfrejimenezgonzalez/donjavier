e35582a1a048b8a88a7a845dadfa24567d990e5b
AfredJaVIERjIMENEZgONZALEZ <alfrejimenezgonzalez@gmail.com>
2016-03-11 20:34:41 +0000

    version 2.0

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -2,8 +2,10 @@ public class HolaMundo {
  
 	public static void main(String[] args) {		
 		System.out.println("Hola Mundo");
+		System.out.println("version mejorada 2.0 ");
+
+		System.out.println("version 2.0 ");
 		
-		System.out.println("version 1.0 ");
 
 	}
  
